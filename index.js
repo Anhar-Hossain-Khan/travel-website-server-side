cfc461202fc984d7e3315ecf1deb891a8b82b608
Anhar-Hossain-Khan <anhar.hossain1997@gmail.com>
2021-11-26 20:15:33 +0600

    server side new updated

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ async function run() {
         res.json(result);
     })
 
-        // DELETE API
+        // DELETE APi
       app.delete('/orderplace/:id', async(req, res)=>{
         const id = req.params.id;
         const query = { _id: ObjectId(id)}
